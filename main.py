cc9e8cbaeff33f748521a2f7ed234dea9e3ea41b
NisharN <nisharr03@gmail.com>
2025-08-31 15:14:44 +0530

    sixth  commit

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
 
-app=FastAPI()
+app = FastAPI()
 
+# Root endpoint
+@app.get("/")
+def root():
+    return {"message": "Medical Diagnosis API is running 🚀"}
+
+# Healthcheck endpoint
 @app.get("/health")
 def healthCheck():
-    return {"message":"ok"}
-
+    return {"message": "ok"}
 
 def main():
     print("Hello from medicalreportdiagnosis!")
-
 
 if __name__ == "__main__":
     main()
